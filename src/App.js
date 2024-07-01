900da14feab90d766184dceccf8228e4b4c9c92e
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-02 00:05:47 +0530

    chore: Update header and footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css"; // Importing the styles.css file
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Welcome to Moviepur</h1>
       </header>
+      <footer>
+        <p className="footer">Footer content here</p>
+      </footer>
     </div>
   );
 }
