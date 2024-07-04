c3b552e0be3e3f8891d556dc4cf891d1da587bcc
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-05 00:16:07 +0530

    chore: Update App.js to include Footer component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./styles.css"; // Importing the styles.css file
 import Header from "./components/Header"; // Importing the Header component
+import Footer from "./components/Footer"; // Importing the Footer component
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
       </div>
-      <footer>
-        <p className="footer">Footer content here</p>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 }
