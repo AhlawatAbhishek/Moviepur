c0c2b24ea61e5d68277ffec6c1add3d9ab8cc55a
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-15 20:04:37 +0530

    chore: Add rating color based on movie rating in MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,16 @@ export default function MovieCard({ movie }) {
   const handleError = (event) => {
     event.target.src = "images/default.jpg";
   };
+
+  const getRatingColor = (rating) => {
+    if (rating >= 8.0) {
+      return "rating-good";
+    } else if (rating >= 5.0 && rating < 8.0) {
+      return "rating-ok";
+    } else {
+      return "rating-bad";
+    }
+  };
   return (
     <div key={movie.id} className="movie-card">
       <img
@@ -15,7 +25,9 @@ export default function MovieCard({ movie }) {
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
-        <p className="movie-card-rating">{movie.rating}</p>
+        <p className={`movie-card-rating ${getRatingColor(movie.rating)}`}>
+          {movie.rating}
+        </p>
         <span>{movie.year}</span>
       </div>
     </div>
