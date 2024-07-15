84bc846bef98065fce94f14c37b2add3273f8e4e
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-15 19:54:08 +0530

    chore: Refactor MoviesGrid component to use MovieCard for rendering movies

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles.css";
 import { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
@@ -12,15 +13,7 @@ export default function MoviesGrid() {
   return (
     <div className="movies-grid">
       {movies.map((movie) => (
-        <div key={movie.id} className="movie-card">
-          <img src={`images/${movie.image}`} alt={movie.title} />
-          <div className="movie-card-info">
-            <h3 className="movie-card-title">{movie.title}</h3>
-            <p className="movie-card-genre">{movie.genre}</p>
-            <p className="movie-card-rating">{movie.rating}</p>
-            <span>{movie.year}</span>
-          </div>
-        </div>
+        <MovieCard movie={movie} key={movie.id}></MovieCard>
       ))}
     </div>
   );
