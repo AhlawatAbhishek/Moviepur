c36e13f5aeb12cb2f6fe6a033ad96281fcb0caf4
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-21 00:05:17 +0530

    chore: Add Watchlist component and update navigation in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import "./styles.css"; // Importing the styles.css file
 import Header from "./components/Header"; // Importing the Header component
 import Footer from "./components/Footer"; // Importing the Footer component
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+        {/* BrowserRouter will also work instead of Router Tag */}
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Movies</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>
